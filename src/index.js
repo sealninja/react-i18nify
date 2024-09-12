44a9386949624edaddf70b1fb53214d9429407c9
Gerben Meyer <gerben.meyer@gmail.com>
2024-09-12 07:34:11 +0200

    new config

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-export { forceComponentsUpdate } from './components/Base';
-export { default as Translate } from './components/Translate';
-export { default as Localize } from './components/Localize';
-export { default as I18n } from './components/I18n';
+export { forceComponentsUpdate } from './components/Base.js';
+export { default as Translate } from './components/Translate.js';
+export { default as Localize } from './components/Localize.js';
+export { default as I18n } from './components/I18n.js';
 export { getLocale, setLocale, setLocaleGetter, getTranslations, setTranslations, setTranslationsGetter, setHandleMissingTranslation, setHandleFailedLocalization } from './lib/settings';
 export { default as translate } from './lib/translate';
 export { default as t } from './lib/translate';
